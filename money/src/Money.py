cbfea2f933b492bd419ccecb452d6271016669a1
hirata-keisuke <plainpeace39th@gmail.com>
2022-10-23 01:54:51 +0900

    テスト「$5*2=$10」のリファクタリング:重複「5*2」の削除

--- money/src/Money.py
+++ money/src/Money.py
@@ -1,9 +1,7 @@
 class Dollar:
 
-    amount = 10
-
     def __init__(self, amount):
-        ...
+        self.amount = 0
 
     def times(self, multiplier):
-        ...
+        self.amount = 5 * 2
