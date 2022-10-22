0f7350ec737819c1e451352d5be5b24e5da037bb
hirata-keisuke <plainpeace39th@gmail.com>
2022-10-23 02:06:34 +0900

    テスト「$5*2=$10」をリファクタリング

--- money/src/Money.py
+++ money/src/Money.py
@@ -1,7 +1,7 @@
 class Dollar:
 
     def __init__(self, amount):
-        self.amount = 0
+        self.amount = amount
 
     def times(self, multiplier):
-        self.amount = 5 * 2
+        self.amount *= multiplier
