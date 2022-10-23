282a8458cf85cc76832cdd9ac1e2ae7fff77e44b
hirata-keisuke <plainpeace39th@gmail.com>
2022-10-23 17:53:35 +0900

    DollarとFrancの重複を無くそうとしたら、循環参照に出くわした。

--- money/src/Money.py
+++ money/src/Money.py
@@ -1,3 +1,7 @@
+from money.src.Dollar import Dollar
+from money.src.Franc import Franc
+from abc import abstractmethod
+
 class Money:
 
     def __init__(self, amount: int):
@@ -7,3 +11,9 @@ class Money:
         if isinstance(other, self.__class__):
             return self.__dict__ == other.__dict__
         return False
+
+    def dollar(amount: int) -> Dollar:
+        return Dollar(amount)
+
+    def franc(amount: int) -> Franc:
+        return Franc(amount)
