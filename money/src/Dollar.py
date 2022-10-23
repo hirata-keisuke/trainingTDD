308da512fa620375d2f2aff04a5dfe90dc02d385
hirata-keisuke <plainpeace39th@gmail.com>
2022-10-23 14:47:23 +0900

    テスト「equals()」で三角測量

--- money/src/Dollar.py
+++ money/src/Dollar.py
@@ -7,4 +7,6 @@ class Dollar:
         return Dollar(self.amount * multiplier)
 
     def __eq__(self, other):
-        return True
+        if isinstance(other, Dollar):
+            return self.__dict__ == other.__dict__
+        return False
