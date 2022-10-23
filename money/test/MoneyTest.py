3bc0c613db49ba4bd2ebdc270836613b91abc675
hirata-keisuke <plainpeace39th@gmail.com>
2022-10-23 09:41:28 +0900

    テスト「Dollarの副作用をどうする？」を作成・失敗

--- money/test/MoneyTest.py
+++ money/test/MoneyTest.py
@@ -1,10 +1,13 @@
 from money.src.Dollar import Dollar
 import unittest
 
-class MoneyTest(unittest.TestCase) :
+class MoneyTest(unittest.TestCase):
 
-    def test_multiplication(self) :
+    def test_multiplication(self):
 
         five = Dollar(5)
         five.times(2)
         self.assertEqual(10, five.amount)
+
+        five.times(3)
+        self.assertEqual(15, five.amount)
