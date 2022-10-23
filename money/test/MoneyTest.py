2b8a11b39f822a21fae7a0298bd8c68d79f46726
hirata-keisuke <plainpeace39th@gmail.com>
2022-10-23 15:02:13 +0900

    テスト「Noneとの等価性比較」を追加・成功

--- money/test/MoneyTest.py
+++ money/test/MoneyTest.py
@@ -15,3 +15,4 @@ class MoneyTest(unittest.TestCase):
     def test_equality(self):
         self.assertTrue(Dollar(5) == Dollar(5))
         self.assertFalse(Dollar(5) == Dollar(6))
+        self.assertFalse(Dollar(5) == None)
