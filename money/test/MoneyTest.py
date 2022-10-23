7f37e641c7c4f340d279ebb7f4d24e3d27e811aa
hirata-keisuke <plainpeace39th@gmail.com>
2022-10-23 09:50:37 +0900

    テスト「Dollarの副作用をどうする？」を成功・リファクタリング

--- money/test/MoneyTest.py
+++ money/test/MoneyTest.py
@@ -6,8 +6,8 @@ class MoneyTest(unittest.TestCase):
     def test_multiplication(self):
 
         five = Dollar(5)
-        five.times(2)
-        self.assertEqual(10, five.amount)
+        product = five.times(2)
+        self.assertEqual(10, product.amount)
 
-        five.times(3)
-        self.assertEqual(15, five.amount)
+        product = five.times(3)
+        self.assertEqual(15, product.amount)
