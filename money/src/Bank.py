f5ef60736473d731b7e065f055216e7e9b94b7a9
hirata-keisuke <plainpeace39th@gmail.com>
2022-10-25 01:12:25 +0900

    何もしていないコンストラクタを消去

--- money/src/Bank.py
+++ money/src/Bank.py
@@ -2,9 +2,6 @@ from money.src.Money import Money
 
 class Bank:
 
-    def __init__(self):
-        pass
-
     def reduce(self, source, to: str):
         return source.reduce(to)
     
--- money/src/Expression.py
+++ money/src/Expression.py
@@ -2,8 +2,5 @@ from abc import ABCMeta
 
 class Expression(metaclass=ABCMeta):
 
-    def __init__(self):
-        pass
-
     def reduce(to: str):
         pass
