eaea04bc2c1c29db3f423124a2bde631838b54e8
hirata-keisuke <plainpeace39th@gmail.com>
2022-10-25 00:43:50 +0900

    Sumオブジェクトのメンバ変数を参照する処理はSumのメソッドへ。循環importは関数内importで解消

--- money/src/Bank.py
+++ money/src/Bank.py
@@ -1,10 +1,7 @@
-from money.src.Money import Money
-
 class Bank:
 
     def __init__(self):
         pass
 
-    def reduce(self, source, to):
-        amount = source.augend._amount + source.addend._amount
-        return Money(amount, to)
+    def reduce(self, source, to: str):
+        return source.reduce(to)
--- money/src/Sum.py
+++ money/src/Sum.py
@@ -5,3 +5,8 @@ class Sum(Expression):
     def __init__(self, augend, addend):
         self.augend = augend
         self.addend = addend
+
+    def reduce(self, to: str):
+        from money.src.Money import Money
+        amount = self.augend._amount + self.addend._amount
+        return Money(amount, to)
